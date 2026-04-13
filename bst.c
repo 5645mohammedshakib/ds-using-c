0c19c217c5c9ed3ab15be99fcd7b420a0d80fbcc
Mohammed shakib <5645mohammedshakib@gmail.com>
2026-04-14 02:55:21 +0530

    Add files via upload

---
+++ bst.c
@@ -0,0 +1,173 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int key;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *root = NULL;
+
+struct node *newNode(int item)
+{
+    struct node *temp = (struct node *)malloc(sizeof(struct node));
+    temp->key = item;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+void inorder(struct node *root)
+{
+    if (root != NULL)
+    {
+        inorder(root->left);
+        printf("%d -> ", root->key);
+        inorder(root->right);
+    }
+}
+
+void preorder(struct node *root)
+{
+    if (root != NULL)
+    {
+        printf("%d -> ", root->key);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void postorder(struct node *root)
+{
+    if (root != NULL)
+    {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d -> ", root->key);
+    }
+}
+
+struct node *insert(struct node *node, int key)
+{
+    if (node == NULL)
+        return newNode(key);
+
+    if (key < node->key)
+        node->left = insert(node->left, key);
+    else
+        node->right = insert(node->right, key);
+
+    return node;
+}
+
+struct node *minValueNode(struct node *node)
+{
+    struct node *current = node;
+
+    while (current && current->left != NULL)
+        current = current->left;
+
+    return current;
+}
+
+struct node *deleteNode(struct node *root, int key)
+{
+    if (root == NULL)
+        return root;
+
+    if (key < root->key)
+        root->left = deleteNode(root->left, key);
+    else if (key > root->key)
+        root->right = deleteNode(root->right, key);
+    else
+    {
+        if (root->left == NULL)
+        {
+            struct node *temp = root->right;
+            free(root);
+            return temp;
+        }
+        else if (root->right == NULL)
+        {
+            struct node *temp = root->left;
+            free(root);
+            return temp;
+        }
+
+        struct node *temp = minValueNode(root->right);
+        root->key = temp->key;
+        root->right = deleteNode(root->right, temp->key);
+    }
+    return root;
+}
+
+void freeMemory(struct node *root)
+{
+    if (root == NULL)
+        return;
+
+    freeMemory(root->left);
+    freeMemory(root->right);
+    free(root);
+}
+
+int main(void)
+{
+    int choice, value;
+    printf("Binary Tree Implementation...!");
+    do
+    {
+        printf("\n1. Insertion");
+        printf("\n2. Deletion");
+        printf("\n3. inorder");
+        printf("\n4. preorder");
+        printf("\n5. postorder");
+        printf("\n6. Exit");
+        printf("\nEnter your choice: ");
+
+        if (scanf("%d", &choice) != 1)
+        {
+            printf("\nWrong selection!!! Try again!!!");
+            while (getchar() != '\n')
+                ;
+            continue;
+        }
+
+        switch (choice)
+        {
+        case 1:
+            printf("Enter the value to be insert: ");
+            scanf("%d", &value);
+            root = insert(root, value);
+            break;
+
+        case 2:
+            printf("Enter the value to be deleted: ");
+            scanf("%d", &value);
+            root = deleteNode(root, value);
+            break;
+
+        case 3:
+            inorder(root);
+            break;
+
+        case 4:
+            preorder(root);
+            break;
+
+        case 5:
+            postorder(root);
+            break;
+
+        case 6:
+            freeMemory(root);
+            break;
+
+        default:
+            printf("\nWrong selection!!! Try again!!!");
+        }
+    } while (choice != 6);
+
+    return 0;
+}
